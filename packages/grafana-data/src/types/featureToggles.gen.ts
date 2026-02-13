dc4bebc0537baaf1300ba0840e659b7c1019feeb
Mihai Doarna <mihai.doarna@grafana.com>
2026-02-13 15:30:35 +0200

    add kubernetesUserSync feature flag

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -1478,6 +1478,11 @@ export interface FeatureToggles {
   */
   kubernetesTeamSync?: boolean;
   /**
+  * Use the new APIs for user sync
+  * @default false
+  */
+  kubernetesUserSync?: boolean;
+  /**
   * Enables the ability to create multiple alerting policies
   * @default false
   */
