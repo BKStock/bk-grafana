c95ddcfda68597a190923830dd80480cc47fbf25
Galen <galen.kistler@grafana.com>
2026-02-26 15:38:10 -0600

    chore: testing wip

---
+++ public/app/plugins/panel/timeseries/plugins/AnnotationsPlugin2.test.tsx
@@ -0,0 +1,127 @@
+import { act, render, screen, waitFor } from '@testing-library/react';
+import React from 'react';
+import uPlot from 'uplot';
+
+import { ScopedVars } from '@grafana/data';
+import { UPlotConfigBuilder } from '@grafana/ui';
+import { TimeRange2 } from '@grafana/ui/internal';
+
+import { AnnotationsPlugin2 } from './AnnotationsPlugin2';
+import { mockAnnotationFrame } from './mockAnnotationFrames';
+
+jest.mock('react-dom', () => ({
+  ...jest.requireActual('react-dom'),
+  // @ts-ignore
+  createPortal: (node) => {
+    return node;
+  },
+}));
+
+jest.mock('uplot', () => {
+  const setDataMock = jest.fn();
+  const setSizeMock = jest.fn();
+  const initializeMock = jest.fn();
+  const destroyMock = jest.fn();
+  const rootElement = jest.fn();
+  const valToPos = jest.fn();
+  const redraw = jest.fn();
+  return jest.fn().mockImplementation(() => {
+    return {
+      setData: setDataMock,
+      setSize: setSizeMock,
+      initialize: initializeMock,
+      destroy: destroyMock,
+      valToPos,
+      redraw,
+      rect: {
+        width: 600,
+      },
+      root: {
+        querySelector: jest.fn().mockImplementation(() => rootElement),
+      },
+    };
+  });
+});
+
+const uplotMock = jest.requireMock('uplot');
+const uplotMockInstance = uplotMock();
+uplotMockInstance.setData.mockImplementationOnce(() => {});
+
+describe('AnnotationsPlugin2', () => {
+  let hooks: Record<string, (u: uPlot) => {}> = {};
+  let config: UPlotConfigBuilder;
+  const setUp = (props?: React.ComponentProps<typeof AnnotationsPlugin2>) => {
+    function applyReady() {
+      act(() => {
+        //@ts-ignore
+        hooks.ready(new uPlot());
+      });
+    }
+
+    const result = render(
+      <AnnotationsPlugin2
+        annotations={[mockAnnotationFrame]}
+        annotationsOptions={{}}
+        config={config}
+        timeZone={'browser'}
+        newRange={{ from: 1759388895560, to: 1759390200000 }}
+        setNewRange={function (newRage: TimeRange2 | null): void {
+          throw new Error('Function not implemented.');
+        }}
+        replaceVariables={function (value: string, scopedVars?: ScopedVars, format?: string | Function): string {
+          throw new Error('Function not implemented.');
+        }}
+        {...props}
+      />
+    );
+
+    applyReady();
+    return result;
+  };
+
+  beforeEach(() => {
+    //@todo remove
+    jest.spyOn(console, 'log').mockImplementation();
+    hooks = {};
+    config = {
+      addHook: jest.fn((type, hook) => {
+        hooks[type] = hook;
+      }),
+    } as unknown as UPlotConfigBuilder;
+  });
+
+  it('should render', async () => {
+    setUp();
+    await waitFor(() => expect(screen.queryAllByTestId('data-testid annotation-marker').length).toEqual(4));
+  });
+
+  it.todo('should display avatar');
+  it.todo('alerts');
+
+  describe('edit', () => {});
+  describe('delete', () => {});
+
+  describe('fields', () => {
+    it.todo('time');
+    it.todo('timeEnd');
+    it.todo('isRegion');
+    it.todo('color');
+    it.todo('title');
+    it.todo('id');
+
+    describe('text', () => {
+      it.todo('links');
+    });
+
+    it.todo('type');
+    it.todo('tags');
+    it.todo('source');
+    it.todo('clusterIdx');
+    it.todo('links');
+  });
+
+  describe('overrides', () => {
+    it.todo('links');
+    it.todo('actions');
+  });
+});
---
+++ public/app/plugins/panel/timeseries/plugins/mockAnnotationFrames.ts
@@ -0,0 +1,107 @@
+import { DataFrame, DataTopic, FieldType } from '@grafana/data';
+
+export const mockAnnotationFrame: DataFrame = {
+  length: 4,
+  meta: {
+    dataTopic: DataTopic.Annotations,
+  },
+  fields: [
+    {
+      name: 'time',
+      config: {},
+      type: FieldType.time,
+      values: [1759388895560, 1759388995560, 1759389995560, 1759390200000],
+    },
+    {
+      name: 'title',
+      config: {},
+      type: FieldType.string,
+      values: ['HG Launch (ops) 1', 'HG Launch (ops) 2', 'HG Launch (ops) 3', 'HG Launch (ops) 4'],
+    },
+    {
+      name: 'text',
+      config: {},
+      type: FieldType.string,
+      values: [
+        'Launching HG Instance ops with hgrun version 1',
+        'Launching HG Instance ops with hgrun version 2',
+        'Launching HG Instance ops with hgrun version 3',
+        'Launching HG Instance ops with hgrun version 4',
+      ],
+    },
+    {
+      name: 'tags',
+      config: {},
+      type: FieldType.other,
+      values: [['tag1', 'tag2'], ['tag2', 'tag3'], [], []],
+    },
+    {
+      name: 'source',
+      config: {},
+      type: FieldType.other,
+      values: [
+        {
+          datasource: {
+            type: 'loki',
+            uid: '000000193',
+          },
+          enable: true,
+          expr: '{cluster="$cluster", namespace="hosted-grafana", slug="$slug"} |= `msg="launching hosted grafana"` | logfmt  ',
+          iconColor: 'dark-purple',
+          instant: false,
+          name: 'HG Launch',
+          tagKeys: 'hg-launch',
+          textFormat: 'Launching HG Instance {{slug}} with hgrun {{version}}',
+          titleFormat: 'HG Launch ({{slug}})',
+        },
+        {
+          datasource: {
+            type: 'loki',
+            uid: '000000193',
+          },
+          enable: true,
+          expr: '{cluster="$cluster", namespace="hosted-grafana", slug="$slug"} |= `msg="launching hosted grafana"` | logfmt  ',
+          iconColor: 'dark-purple',
+          instant: false,
+          name: 'HG Launch',
+          tagKeys: 'hg-launch',
+          textFormat: 'Launching HG Instance {{slug}} with hgrun {{version}}',
+          titleFormat: 'HG Launch ({{slug}})',
+        },
+        {
+          datasource: {
+            type: 'loki',
+            uid: '000000193',
+          },
+          enable: true,
+          expr: '{cluster="$cluster", namespace="hosted-grafana", slug="$slug"} |= `msg="launching hosted grafana"` | logfmt  ',
+          iconColor: 'dark-purple',
+          instant: false,
+          name: 'HG Launch',
+          tagKeys: 'hg-launch',
+          textFormat: 'Launching HG Instance {{slug}} with hgrun {{version}}',
+          titleFormat: 'HG Launch ({{slug}})',
+        },
+        null,
+      ],
+    },
+    {
+      name: 'color',
+      config: {},
+      type: FieldType.string,
+      values: ['#8F3BB8', '#8F3BB8', '#8F3BB8', '#8F3BB8'],
+    },
+    {
+      name: 'type',
+      config: {},
+      type: FieldType.string,
+      values: ['HG Launch 1', 'HG Launch 2', 'HG Launch 3', 'HG Launch 4'],
+    },
+    {
+      name: 'isRegion',
+      config: {},
+      type: FieldType.boolean,
+      values: [false, false, false, false],
+    },
+  ],
+};
