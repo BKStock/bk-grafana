2b96afc2709c282bcd52047e16d5f152177cf003
Haris Rozajac <haris.rozajac12@gmail.com>
2026-02-27 14:44:42 -0700

    revert changes as they don't apply anymore

--- public/app/features/dashboard-scene/settings/links/DashboardLinkForm.tsx
+++ public/app/features/dashboard-scene/settings/links/DashboardLinkForm.tsx
@@ -2,7 +2,7 @@ import { css } from '@emotion/css';
 import * as React from 'react';
 
 import { SelectableValue } from '@grafana/data';
-import { t } from '@grafana/i18n';
+import { t, Trans } from '@grafana/i18n';
 import { DashboardLink } from '@grafana/schema';
 import { CollapsableSection, TagsInput, Select, Field, Input, Checkbox, Button, Stack, useStyles2 } from '@grafana/ui';
 
@@ -14,11 +14,9 @@ interface DashboardLinkFormProps {
   link: DashboardLink;
   onUpdate: (link: DashboardLink) => void;
   onGoBack: () => void;
-  /** When set, shows "Add link" (primary) and "Cancel" instead of "Back to list" (e.g. for inline add from side pane) */
-  onAdd?: () => void;
 }
 
-export function DashboardLinkForm({ link, onUpdate, onGoBack, onAdd }: DashboardLinkFormProps) {
+export function DashboardLinkForm({ link, onUpdate, onGoBack }: DashboardLinkFormProps) {
   const styles = useStyles2(getStyles);
   const linkTypeOptions = [
     {
@@ -177,16 +175,9 @@ export function DashboardLinkForm({ link, onUpdate, onGoBack, onAdd }: Dashboard
           </Stack>
         </CollapsableSection>
 
-        <div style={{ display: 'flex', gap: '8px', flexWrap: 'wrap' }}>
-          {onAdd && (
-            <Button variant="primary" onClick={onAdd}>
-              {t('dashboard-scene.dashboard-link-form.add-link', 'Add link')}
-            </Button>
-          )}
+        <div>
           <Button variant="secondary" onClick={onGoBack}>
-            {onAdd
-              ? t('dashboard-scene.dashboard-link-form.cancel', 'Cancel')
-              : t('dashboard-scene.dashboard-link-form.back-to-list', 'Back to list')}
+            <Trans i18nKey="dashboard-scene.dashboard-link-form.back-to-list">Back to list</Trans>
           </Button>
         </div>
       </Stack>
