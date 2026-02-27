0d43bf9fddf0ebddc4053620a538b36eb3d2ed3e
L2D2Grafana <liza.detrick@grafana.com>
2026-02-27 13:42:32 -0800

    Explore Logs: mock msw

--- public/app/plugins/datasource/loki/jest-setup.js
+++ public/app/plugins/datasource/loki/jest-setup.js
@@ -1,1 +1,18 @@
 import '@grafana/plugin-configs/jest/jest-setup';
+
+// Polyfills for MSW (used by setTestFlags in querySplitting.test.ts)
+import 'whatwg-fetch';
+
+const globalObj = typeof globalThis !== 'undefined' ? globalThis : global;
+if (typeof globalObj.BroadcastChannel === 'undefined') {
+  globalObj.BroadcastChannel = class BroadcastChannel {
+    constructor() {}
+    postMessage() {}
+    close() {}
+    addEventListener() {}
+    removeEventListener() {}
+    dispatchEvent() {
+      return true;
+    }
+  };
+}
