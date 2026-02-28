765783d39f45057da6d1e2a6d32f01e53429d4a6
Adela Almasan <adela.almasan@grafana.com>
2026-02-27 20:51:51 -0600

    update presets header

--- public/app/features/panel/components/VizTypePicker/VisualizationPresets.tsx
+++ public/app/features/panel/components/VizTypePicker/VisualizationPresets.tsx
@@ -1,9 +1,8 @@
 import { css } from '@emotion/css';
 
 import { GrafanaTheme2, PanelData, PanelPluginVisualizationSuggestion } from '@grafana/data';
-import { selectors } from '@grafana/e2e-selectors';
 import { t, Trans } from '@grafana/i18n';
-import { Button, IconButton, Text, useStyles2 } from '@grafana/ui';
+import { Button, Text, useStyles2 } from '@grafana/ui';
 
 import { VisualizationCardGrid } from './VisualizationCardGrid';
 
@@ -21,21 +20,17 @@ export function VisualizationPresets({ presets, data, onApply, onBack, onSkip }:
   return (
     <div className={styles.wrapper}>
       <div className={styles.header}>
-        <div className={styles.headerLeft}>
-          <IconButton
-            key="arrow-left"
-            name="arrow-left"
-            variant="primary"
-            size="xl"
-            aria-label={t('panel.presets.back-button', 'Back')}
-            tooltip={t('panel.presets.back-button-tooltip', 'Go back')}
-            data-testid={selectors.components.PanelEditor.toggleVizPicker}
-            onClick={onBack}
-          />
-          <Text element="p" variant="body">
-            <Trans i18nKey="panel.presets.title">Select style</Trans>
-          </Text>
-        </div>
+        <Button
+          fill="text"
+          icon="arrow-left"
+          aria-label={t('panel.presets.back-button-text', 'Go back')}
+          onClick={onBack}
+        >
+          <Trans i18nKey="panel.presets.back-button-text">Go back</Trans>
+        </Button>
+        <Text element="p" variant="body">
+          <Trans i18nKey="panel.presets.title">Select style</Trans>
+        </Text>
         <Button variant="primary" fill="text" onClick={onSkip}>
           <Trans i18nKey="panel.presets.skip-button">Skip</Trans>
         </Button>
